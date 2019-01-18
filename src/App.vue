dd53dc36beb8d0179ee8d9a17fddce888eb897e7
mwvalen <mwillvalentine@gmail.com>
2019-01-18 15:35:27 -0500

    Vuetify added + default content replaced

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,40 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <router-link to='/' class='link'>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>HackerNews</span>
+        <span class="font-weight-light">LITE</span>
+      </v-toolbar-title>
+      </router-link>
+      <v-spacer></v-spacer>
+        <router-link to='/about' class='link'>
+          <span class="mr-2">About</span>
+        </router-link>
+    </v-toolbar>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      //
     }
   }
 }
+</script>
+
+<style scoped>
+  .link {
+    color: black;
+    text-decoration: none;
+  }
 </style>
+
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container fluid>
+    <v-layout row wrap text-xs-center>
+      <v-flex>
+        <h1 class="display-3 font-weight-thin">About</h1>
+        <p>Hacker News without external links</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
